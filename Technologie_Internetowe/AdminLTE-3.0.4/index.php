c7ce624aa2e5b613c4d54ee740f9b71141369874
MichalWisniowski98 <michalwisniowski98@gmail.com>
2020-06-03 12:48:05 +0200

    szóste_zajęcia_03.06

--- Technologie_Internetowe/AdminLTE-3.0.4/index.php
+++ Technologie_Internetowe/AdminLTE-3.0.4/index.php
@@ -21,7 +21,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="./"><b>Admin</b>LTE</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
@@ -77,7 +77,7 @@
         <a href="./pages/forgot-password.html">I forgot my password</a>
       </p>
       <p class="mb-0">
-        <a href="./pages/register.html" class="text-center">Register a new membership</a>
+        <a href="./pages/register.php" class="text-center">Register a new membership</a>
       </p>
     </div>
     <!-- /.login-card-body -->
--- Technologie_Internetowe/AdminLTE-3.0.4/pages/register.php
+++ Technologie_Internetowe/AdminLTE-3.0.4/pages/register.php
@@ -1,3 +1,6 @@
+<?php
+  session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,16 +24,29 @@
 <body class="hold-transition register-page">
 <div class="register-box">
   <div class="register-logo">
-    <a href="../../index2.html"><b>Admin</b>LTE</a>
+    <a href="../"><b>Admin</b>LTE</a>
   </div>
+
+  <?php
+    if(isset($_SESSION['error'])){
+    echo<<<ERROR
+      <div class="card card-outline card-danger">
+        <div class="card-header">
+          <h3 class="card-title">{$_SESSION['error']}</h3>
+        </div>
+      <div
+    ERROR;
+      unset($_SESSION['error']);
+    }
+  ?>
 
   <div class="card">
     <div class="card-body register-card-body">
       <p class="login-box-msg">Register a new membership</p>
 
-      <form action="../../index.html" method="post">
+      <form action="../scripts/add_user.php" method="post">
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Imie">
+          <input type="text" class="form-control" placeholder="Imie" name="name">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
@@ -38,7 +54,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Nazwisko">
+          <input type="text" class="form-control" placeholder="Nazwisko" name="surname">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
@@ -46,7 +62,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="email" class="form-control" placeholder="Email" name="email1">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -54,7 +70,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Retype Email">
+          <input type="email" class="form-control" placeholder="Retype Email" name="email2">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -62,7 +78,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" class="form-control" placeholder="Password" name="pass1">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -70,7 +86,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
+          <input type="password" class="form-control" placeholder="Retype password" name="pass2">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -78,7 +94,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="date" class="form-control" placeholder="rrrr-mm-dd">
+          <input type="date" class="form-control" placeholder="rrrr-mm-dd" name="birthday">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="nav-icon far fa-calendar-alt"></span>
